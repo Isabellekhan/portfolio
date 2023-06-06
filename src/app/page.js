9aa7a0d71f629e344f119e38b3d83935b0a9557a
isabelle khan <mahk-06@hotmail.com>
2023-06-07 01:32:52 +0200

    adding padding

--- src/app/page.js
+++ src/app/page.js
@@ -129,7 +129,7 @@ export default function Home() {
             </div>
             <div className="basis-1/3 flex-1">
               <h4 className="font-bold py-2">Quote generator</h4>
-              <p>
+              <p className="text-md py-5 ">
                 This project is a simple quote generator, that is fetching an
                 api to generat these quotes
               </p>
